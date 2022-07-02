63e0fca8a19f083b2848a8f5ffe1cb721dbcb908
Fernando Lagomarsino <77158986+intronic-ar@users.noreply.github.com>
2022-07-02 16:56:24 -0300

    Add files via upload

---
+++ Bode.m
@@ -0,0 +1,109 @@
+%{
+clear, clc
+
+% R-C Pasa altos 
+
+C = 0.1*10^-6;
+R = 1000;
+Num = [1 0];
+Den = [1 1/(R*C) ]
+G = tf( Num, Den)
+bode(G)
+%}
+
+
+%{
+clear, clc
+
+% R-C Pasa bajos 
+
+C = 0.1*10^-6;
+R = 1000;
+Num = [ 1/(R*C) ];
+Den = [1 1/(R*C)]
+G = tf( Num, Den)
+bode(G)
+%}
+
+%{
+clear, clc
+
+% R-C Pasa banda
+
+C = 0.1*10^-6;
+R = 1000;
+k = 2;
+
+Num = [ k/(R*C) 0 ];
+Den = [ 1 2/(R*C) 1/(R*C)^2]
+G = tf( Num, Den)
+bode(G)
+%}
+
+%{
+clear, clc
+
+% R-C Pasa banda distintos componentes
+
+C1 = 0.1*10^-6;
+R1 = 1000;
+C2 = 0.01*10^-6;
+R2 = 1000;
+
+k = 5;
+
+Num = [ k/(R1*C1) 0 ];
+Den = [ 1 ((R1*C1+R2*C2)/(R1*C1*R2*C2)) 1/(R1*C1*R2*C2)]
+G = tf( Num, Den)
+bode(G)
+%}
+
+%{
+clear, clc
+
+% R-C rechaza banda (Notch) distintos componentes
+
+C1 = 0.1*10^-6;
+R1 = 1000;
+C2 = 0.1*10^-6;
+R2 = 1000;
+
+k = 1;
+
+Num = [ k k*((R1*C1+R2*C2)/(R1*C1*R2*C2)) k/(R1*C1*R2*C2) ];
+Den = [ 1/(R1*C1) 0 ]
+G = tf( Num, Den)
+bode(G)
+%}
+
+%{
+clear, clc
+
+% R-L-C Pasa bajos
+
+C = 0.1*10^-6;
+L = 1*10^-3;
+R = 1.23;
+
+Num = [ 1/(L*C) ];
+Den = [ 1 R/L 1/(L*C) ]
+G = tf( Num, Den)
+bode(G)
+
+%}
+
+
+clear, clc
+
+% L-C Pasa bajos amortiguado
+
+C = 0.1*10^-6;
+RD = 100;
+CD = 0.4*10^-6;
+L = 1*10^-3;
+
+Num = [ RD*CD 1 ];
+Den = [ C*CD*L*RD C*L+CD*L RD*CD 1 ]
+G = tf( Num, Den)
+bode(G)
+
